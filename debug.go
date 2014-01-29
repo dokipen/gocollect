c2815308fc4ab03b8d79435fd96e67169d29c40e
Bob Corsaro <bob@embed.ly>
2014-01-29 10:22:53 -0500

    Make reads threadsafe

--- debug.go
+++ debug.go
@@ -1,79 +1,81 @@
 package debug
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
-    "strings"
-    "github.com/wsxiaoys/terminal/color"
-    "crypto/md5"
-    "io"
-    "sync"
+	"crypto/md5"
+	"fmt"
+	"github.com/wsxiaoys/terminal/color"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
 const COLORS = "rgbcmykw"
 
 var (
-    match_cache  = map[string]bool{}
-    prefix_cache = map[string]string{}
-    match_mutex = new(sync.Mutex)
-    prefix_mutex = new(sync.Mutex)
+	match_cache  = map[string]bool{}
+	prefix_cache = map[string]string{}
+	match_mutex  = new(sync.Mutex)
+	prefix_mutex = new(sync.Mutex)
 )
 
 func match(namespace string) (match bool) {
-    var ok bool
-    if match, ok = match_cache[namespace]; !ok {
-        selectors := strings.Split(os.Getenv("DEBUG"), " ")
-        for selector := range selectors {
-            if ok, _ = filepath.Match(selectors[selector], namespace); ok  {
-                match = true
-                match_mutex.Lock()
-                defer match_mutex.Unlock()
+	var ok bool
+
+    match_mutex.Lock()
+	defer match_mutex.Unlock()
+
+	if match, ok = match_cache[namespace]; !ok {
+		selectors := strings.Split(os.Getenv("DEBUG"), " ")
+		for selector := range selectors {
+			if ok, _ = filepath.Match(selectors[selector], namespace); ok {
+				match = true
                 match_cache[namespace] = match
-                return
-            }
-        }
-        match = false
-        match_cache[namespace] = match
-    }
-    return
+				return
+			}
+		}
+		match = false
+		match_cache[namespace] = match
+	}
+	return
 }
 
 func getcolor(namespace string) string {
-    h := md5.New()
-    io.WriteString(h, namespace)
-    var sum int
-    sumbytes := h.Sum(nil)
-    for i := range sumbytes {
-        sum += int(sumbytes[i])
-    }
-    return fmt.Sprintf("%c", COLORS[sum % len(COLORS)])
+	h := md5.New()
+	io.WriteString(h, namespace)
+	var sum int
+	sumbytes := h.Sum(nil)
+	for i := range sumbytes {
+		sum += int(sumbytes[i])
+	}
+	return fmt.Sprintf("%c", COLORS[sum%len(COLORS)])
 
 }
 
 func printns(namespace string) {
-    var ok bool
-    var prefix string
+	var ok bool
+	var prefix string
 
-    if prefix, ok = prefix_cache[namespace]; !ok {
-        prefix = fmt.Sprintf("@%s%s@| ", getcolor(namespace), namespace)
-        prefix_mutex.Lock()
-        defer prefix_mutex.Unlock()
-        prefix_cache[namespace] = prefix
-    }
+	prefix_mutex.Lock()
+	if prefix, ok = prefix_cache[namespace]; !ok {
+		prefix = fmt.Sprintf("@%s%s@| ", getcolor(namespace), namespace)
+		prefix_cache[namespace] = prefix
+	}
+	prefix_mutex.Unlock()
 
-    color.Print(prefix)
+	color.Print(prefix)
 }
 
 func Log(namespace, msg string, args ...interface{}) {
-    if match(namespace) {
-        printns(namespace)
-        fmt.Printf(msg, args...)
-    }
+	if match(namespace) {
+		printns(namespace)
+		fmt.Printf(msg, args...)
+	}
 }
 
-func Logger(namespace string) func (string, ...interface{}) {
-    return func (msg string, args ...interface{}) {
-        Log(namespace, msg, args...)
-    }
+func Logger(namespace string) func(string, ...interface{}) {
+	return func(msg string, args ...interface{}) {
+		Log(namespace, msg, args...)
+	}
 }
