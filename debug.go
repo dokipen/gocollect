257eeffb57eb2dc131f1b67936725c63cd70c33f
Bob Corsaro <bob@embed.ly>
2014-01-29 09:58:55 -0500

    init

---
+++ debug.go
@@ -0,0 +1,72 @@
+package debug
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+    "github.com/wsxiaoys/terminal/color"
+    "crypto/md5"
+    "io"
+)
+
+const COLORS = "rgbcmykw"
+
+var (
+    match_cache  = map[string]bool{}
+    prefix_cache = map[string]string{}
+)
+
+func match(namespace string) (match bool) {
+    var ok bool
+    if match, ok = match_cache[namespace]; !ok {
+        selectors := strings.Split(os.Getenv("DEBUG"), " ")
+        for selector := range selectors {
+            if ok, _ = filepath.Match(selectors[selector], namespace); ok  {
+                match = true
+                match_cache[namespace] = match
+                return
+            }
+        }
+        match = false
+        match_cache[namespace] = match
+    }
+    return
+}
+
+func getcolor(namespace string) string {
+    h := md5.New()
+    io.WriteString(h, namespace)
+    var sum int
+    sumbytes := h.Sum(nil)
+    for i := range sumbytes {
+        sum += int(sumbytes[i])
+    }
+    return fmt.Sprintf("%c", COLORS[sum % len(COLORS)])
+
+}
+
+func printns(namespace string) {
+    var ok bool
+    var prefix string
+
+    if prefix, ok = prefix_cache[namespace]; !ok {
+        prefix = fmt.Sprintf("@%s%s@| ", getcolor(namespace), namespace)
+        prefix_cache[namespace] = prefix
+    }
+
+    color.Print(prefix)
+}
+
+func Log(namespace, msg string, args ...interface{}) {
+    if match(namespace) {
+        printns(namespace)
+        fmt.Printf(msg, args...)
+    }
+}
+
+func Logger(namespace string) func (string, ...interface{}) {
+    return func (msg string, args ...interface{}) {
+        Log(namespace, msg, args...)
+    }
+}
