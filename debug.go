336da334f675ad8f2e7d041ccbcf8a5df0b7cba5
Bob Corsaro <bob@embed.ly>
2014-03-13 17:25:16 -0400

    go fmt

--- debug.go
+++ debug.go
@@ -14,18 +14,19 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-    "time"
+	"time"
 )
 
 // Possible color values.
 const COLORS = "rgbcmykw"
+
 // Formatted in two stages. First (color, namespace, color) which is cached,
 // then (file, linenum, message, elapsetime) for the actual message.
 const FMT = "  @%s%s@| @!%%s:%%d@| @w%%s@| @%s+%%s@|\n"
 
 type message struct {
-    from_file string
-    from_line int
+	from_file string
+	from_line int
 	namespace string
 	message   string
 	args      []interface{}
@@ -34,28 +35,27 @@ type message struct {
 type messageChan chan *message
 
 var (
-	fmtCache    = map[string]string{}
-	globs       []string
-    msgCh       = make(messageChan)
+	fmtCache = map[string]string{}
+	globs    []string
+	msgCh    = make(messageChan)
 )
 
-
 func (msg *message) log(elapsed time.Duration) {
-    if fmtstr := formatFor(msg.namespace); fmtstr != "" {
-        payload := fmt.Sprintf(msg.message, msg.args...)
-        content := fmt.Sprintf(fmtstr, msg.from_file, msg.from_line, payload, elapsed)
-        colorfmt.Print(content)
+	if fmtstr := formatFor(msg.namespace); fmtstr != "" {
+		payload := fmt.Sprintf(msg.message, msg.args...)
+		content := fmt.Sprintf(fmtstr, msg.from_file, msg.from_line, payload, elapsed)
+		colorfmt.Print(content)
 	}
 }
 
 func worker(ch messageChan) {
-    last := time.Now()
-    for {
-        msg := <-ch
-        elapsed := time.Since(last)
-        last = time.Now()
-        msg.log(elapsed)
-    }
+	last := time.Now()
+	for {
+		msg := <-ch
+		elapsed := time.Since(last)
+		last = time.Now()
+		msg.log(elapsed)
+	}
 }
 
 /*
@@ -68,7 +68,7 @@ func init() {
 	} else {
 		globs = strings.Split(debug, " ")
 	}
-    go worker(msgCh)
+	go worker(msgCh)
 }
 
 /*
@@ -78,18 +78,18 @@ cache for the namespace to speed things up.
 func formatFor(namespace string) string {
 	var ok bool
 
-    if match, ok := fmtCache[namespace]; ok {
+	if match, ok := fmtCache[namespace]; ok {
 		return match
 	}
 
 	for _, glob := range globs {
 		if ok, _ = filepath.Match(glob, namespace); ok {
-            color := colorFor(namespace)
-            fmtCache[namespace] = fmt.Sprintf(FMT, color, namespace, color)
+			color := colorFor(namespace)
+			fmtCache[namespace] = fmt.Sprintf(FMT, color, namespace, color)
 			return fmtCache[namespace]
 		}
 	}
-    fmtCache[namespace] = ""
+	fmtCache[namespace] = ""
 	return fmtCache[namespace]
 }
 
@@ -113,15 +113,15 @@ func colorFor(namespace string) string {
 Log sends the log message to the log worker to be written to stdout.
 */
 func Log(namespace, msg string, args ...interface{}) {
-    _, path, line, _ := runtime.Caller(2)
-    message := &message{
-        from_file: filepath.Base(path),
-        from_line: line,
-        namespace: namespace,
-        message: msg,
-        args: args,
-    }
-    msgCh <- message
+	_, path, line, _ := runtime.Caller(2)
+	message := &message{
+		from_file: filepath.Base(path),
+		from_line: line,
+		namespace: namespace,
+		message:   msg,
+		args:      args,
+	}
+	msgCh <- message
 }
 
 /*
