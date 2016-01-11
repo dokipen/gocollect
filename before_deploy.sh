9914bbeba4b3f3c1f6bb9613c3255c5ce705e78e
Bob Corsaro <bob@embed.ly>
2016-01-11 09:06:26 -0500

    Build stuff

---
+++ before_deploy.sh
@@ -0,0 +1,12 @@
+mkdir debroot
+cp debian/* debroot -r
+mkdir debroot/usr/bin -p
+cp gocollect debroot/usr/bin
+fmp \
+  -s dir \
+  -t deb \
+  --force \
+  -C "debroot" \
+  -a "amd64" \
+  --version "0.0.1" \
+  .
