429dfad82dcff0e46fb7cc26e8fe17bcf7c192cb
Bob Corsaro <bob@embed.ly>
2016-01-11 10:13:29 -0500

    Improving naming and versioning

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -e
 
+VERSION="0.0.1"
+NAME="gocollect"
+
+if [[ -n "${TRAVIS_BUILD_NUMBER}" ]]; then
+    VERSION="${VERSION}-r${TRAVIS_BUILD_NUMBER}"
+fi
+
+if [[ "${TRAVIS_BRANCH}" != "production" ]]; then
+    NAME="${TRAVIS_BRANCH}-${NAME}"
+fi
+
 mkdir debroot -p
 cp debian/* debroot -r
 mkdir debroot/usr/bin -p
 cp "${GOPATH}/bin/gocollect" debroot/usr/bin
 gem install fpm
+
 fpm \
   -s dir \
   -t deb \
   --force \
   -C "debroot" \
   -a "amd64" \
-  -n "gocollect" \
-  --version "0.0.1" \
+  -n "${NAME}" \
+  --version "${VERSION}" \
   .
